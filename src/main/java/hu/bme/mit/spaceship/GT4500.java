495707949dd0c6b619a09e75a3e208e22dc8d06c
Schmelczer András <andras@schmelczer.dev>
2020-05-06 20:11:54 +0200

    Fix bug

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,7 +77,9 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        firingSuccess = primaryTorpedoStore.fire(1) || secondaryTorpedoStore.fire(1);
+        boolean firingSuccessPrimary = primaryTorpedoStore.fire(1);
+        boolean firingSuccessSecondary = secondaryTorpedoStore.fire(1);
+        firingSuccess = firingSuccessPrimary || firingSuccessSecondary;
         break;
     }
 
